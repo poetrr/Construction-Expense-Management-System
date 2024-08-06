b28ea9d284b7ac2cdf8f654681c4823045a2c629
poetrr <sidducit012@gmail.com>
2024-08-06 18:11:31 +0530

    added watertank

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,16 @@ import { EventBindingComponent } from "../event-binding/event-binding.component"
 import { WatertankComponent } from "../watertank/watertank.component";
 import { BricksComponent } from "../bricks/bricks.component";
 import { DirectivesComponent } from "../directives/directives.component";
+import { SidenavComponent } from "../sidenav/sidenav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PracticeComponent, FormsModule, EventBindingComponent, WatertankComponent, BricksComponent, DirectivesComponent],
+  imports: [FormsModule,RouterOutlet, PracticeComponent, FormsModule, EventBindingComponent, WatertankComponent, BricksComponent, DirectivesComponent, SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project';
+ 
   
 }
