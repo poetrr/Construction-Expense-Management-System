ed7b81a402a08c3451b6b946d61137090b99270c
poetrr <sidducit012@gmail.com>
2024-08-08 20:28:47 +0530

    added sidenavbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PracticeComponent } from "../practice/practice.component";
-import { FormsModule } from '@angular/forms';
-import { EventBindingComponent } from "../event-binding/event-binding.component";
 import { WatertankComponent } from "../watertank/watertank.component";
-import { BricksComponent } from "../bricks/bricks.component";
-import { DirectivesComponent } from "../directives/directives.component";
-import { SidenavComponent } from "../sidenav/sidenav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule,RouterOutlet, PracticeComponent, FormsModule, EventBindingComponent, WatertankComponent, BricksComponent, DirectivesComponent, SidenavComponent],
+  imports: [RouterOutlet, WatertankComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
- 
-  
+  title = 'trialapp';
 }
